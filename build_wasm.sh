558194f88e7a1549eb4e40fc8c004f0ecbdd9515
Mateusz Palkowski <mateusz.palkowski@gmail.com>
2023-04-16 12:25:28 +0200

    Add fetching map data to build script

--- build_wasm.sh
+++ build_wasm.sh
@@ -19,6 +19,24 @@ BUILD_DIR="build/wasm"
 # Move to the project root directory
 cd "$ROOT_DIR" || error_exit "Failed to change directory to project root"
 
+# Run python script to fetch map data
+MAP_DATA_DIR="map_data"
+cd "$MAP_DATA_DIR" || error_exit "Failed to change directory to $MAP_DATA_DIR"
+# Create python venv if it doesn't exist
+if [ ! -d "venv" ]; then
+    python3 -m venv venv || error_exit "Failed to create python venv"
+fi
+# Activate venv
+source venv/bin/activate || error_exit "Failed to activate python venv"
+# Install dependencies
+pip install -r requirements.txt || error_exit "Failed to install python dependencies"
+# Run script
+python3 prepare_map_data.py || error_exit "Failed to run prepare_map_data.py"
+# Deactivate venv
+deactivate || error_exit "Failed to deactivate python venv"
+cd "$ROOT_DIR" || error_exit "Failed to change directory to project root"
+
+
 # Build the WebAssembly project
 mkdir -p "$BUILD_DIR" || error_exit "Failed to create build directory"
 cd "$BUILD_DIR" || error_exit "Failed to change directory to $BUILD_DIR"
